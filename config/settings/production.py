919f9b2aa47a4c81b4d4320291c5c2d8c1ae9cad
steve <stevetasticsteve@gmail.com>
2022-01-20 15:19:58 +0100

    https settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,3 +4,10 @@ import os
 DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY", "Secure_secret_key")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+# HTTPS
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_HSTS_SECONDS = 3600
