a52c99c8e407e34ee805d0101aa33c5bf1644ac0
steve <stevetasticsteve@gmail.com>
2022-01-20 13:08:24 +0100

    getting ready for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,8 +1,6 @@
 from .base import *
+import os
 
 DEBUG = False
-
-try:
-    from .local import *
-except ImportError:
-    pass
+SECRET_KEY = os.environ.get("SECRET_KEY", "Secure_secret_key")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
