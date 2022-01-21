318df5cd651be772c08ed9efe1090e88171ba3b2
steve <stevetasticsteve@gmail.com>
2022-01-21 10:38:01 +0100

    included sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,3 +11,20 @@ SESSION_COOKIE_SECURE = True
 # SECURE_SSL_REDIRECT = True
 # SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # SECURE_HSTS_SECONDS = 3600
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://9c2cc8540b8d426ab5de7989c4146d14@o538547.ingest.sentry.io/6160548",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
